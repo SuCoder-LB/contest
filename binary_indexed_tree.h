7268bdda07438be92f4a5ad72f263b65101d88d0
su_li_biao <sucoder_lb@outlook.com>
2022-02-20 10:24:34 +0800

    Changes

--- binary_indexed_tree.h
+++ binary_indexed_tree.h
@@ -13,15 +13,15 @@ using namespace std;
 struct BinaryIndexedTree {
   vector<long long> s;
   BinaryIndexedTree(int n) : s(n) {}
-  void update(int pos, long long dif) { // a[pos] += dif
+  void Update(int pos, long long dif) { // a[pos] += dif
     for (; pos < s.size(); pos |= pos + 1) s[pos] += dif;
   }
-  long long query(int pos) { // sum of values in [0, pos)
+  long long Query(int pos) { // sum of values in [0, pos)
     long long res = 0;
     for (; pos > 0; pos &= pos - 1) res += s[pos-1];
     return res;
   }
-  int lower_bound(long long sum) {// min pos st sum of [0, pos] >= sum
+  int LowerBound(long long sum) {// min pos st sum of [0, pos] >= sum
     // Returns n if no sum is >= sum, or -1 if empty sum is.
     if (sum <= 0) return -1;
     int pos = 0;
